12bb13b43fbe8a794d30e7aea4137dc5b05b6821
Edmund Teo <Edmund@edmunds-mac.mbox.com>
2017-12-10 09:36:14 +0800

    editing

---
+++ ex.js
@@ -0,0 +1,10 @@
+var data = {
+  name: 'john',
+  age: 21
+}
+
+console.log('something');
+
+module.exports.data = data;
+
+dong dong
---
+++ main.js
@@ -0,0 +1,3 @@
+var newP = $('<p>');
+$('.container').append(newP);
+newP.append('Hello JS!');
